187225aed19b47f0058e937b16d1f31da0aea2b1
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-02-04 12:33:07 +0530

    binary search

---
+++ Searching/binarySearching.java
@@ -0,0 +1,30 @@
+class BinarySearchEx{
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        int key = 40;
+        binarySearch(arr,key);
+    }
+    static void binarySearch(int[] arr, int key){
+        int first = 0;
+        int last = arr.length-1;
+
+        int mid = (first+last)/2;
+        while(first<=last){
+            if(arr[mid]<key){
+                first = mid+1;
+            }
+            else if(arr[mid]==key){
+                System.out.println("Element is at index: "+mid);
+                break;
+            }
+            else{
+                last=mid-1;
+
+            }
+            mid = (first+last)/2;
+        }
+        if(first>last){
+            System.out.println("Element is not found");
+        }
+    }
+}
