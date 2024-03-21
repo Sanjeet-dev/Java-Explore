748643861ef05275c056a08639ccfd9f21465b6c
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-03-21 11:15:43 +0530

    using bits to execute the solution

--- Arrays/missingNumber.java
+++ Arrays/missingNumber.java
@@ -2,7 +2,10 @@ package Arrays;
 class missingNumber{
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,10};
+        int N = arr.length;
         missingInt(arr,10);
+        int ans = missingNum(arr, N);
+        System.out.print(ans);
 
     }
     static void missingInt(int[] arr,int num){
@@ -17,5 +20,17 @@ class missingNumber{
         System.out.println("The missing number is "+ (int)result);
 
 
+    }
+    public static int missingNum(int[] num, int N){
+        int xor1=0;
+        int xor2=0;
+        int n = N-1;
+
+        for(int i=0; i<n; i++){
+            xor2 = xor2^num[i];
+            xor1 = xor1^(i+1);
+        }
+        xor1 = xor1 ^ N;
+        return xor1^xor2;
     }
 }
